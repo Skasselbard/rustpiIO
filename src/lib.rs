3e64a8498b4e58588b9e84a6e5865b5e1f02c680
tom <tm219@uni-rostock.de>
2017-08-12 18:09:52 +0200

    Implemented Read Trait and converge more to better rust code... (Ithink)^^

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@ pub mod rustpi_io{
     use std::fs::OpenOptions;
     use std::io::prelude::*;
     use std::error::Error;
+    use std::io::Result;
+
 
     static GPIO_PATH: &'static str = "/sys/class/gpio/";
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone, Copy)]
     pub enum GPIOMode{
         Read,
         Write
@@ -18,16 +20,13 @@ pub mod rustpi_io{
     }
 
     impl GPIO{
-        pub fn gpio(&self) -> u8 { self.pin }
+        pub fn gpio(self) -> u8 { self.pin }
 
-        pub fn currentMode(&self) -> GPIOMode {
-            match self.mode {
-                GPIOMode::Read => GPIOMode::Read,
-                GPIOMode::Write => GPIOMode::Write
-            }
+        pub fn current_mode(self) -> GPIOMode {
+            self.mode
         }
 
-        pub fn setMode(&self, mode: GPIOMode) -> GPIO {
+        pub fn set_mode(self, mode: GPIOMode) -> Result<Self>{
             let mut direction_file = match OpenOptions::new().write(true).open(format!("{}gpio{}/direction", GPIO_PATH, self.pin)) {
                 Ok(f) => f,
                 Err(e) => panic!("file error: {}", e),
@@ -50,32 +49,59 @@ pub mod rustpi_io{
                         Ok(_) => println!("gpio {} set to write mode", self.pin),
                     }
                 }
-            }
-            GPIO {pin: self.pin, mode: mode}
+            };
+            Ok(Self { pin: self.pin, mode: mode})
         }
 
-        pub fn init(gpio: u8, mode: GPIOMode) -> GPIO {
+        pub fn init(self, gpio: u8, mode: GPIOMode) -> Result<Self> {
             println!("path = {:?}", GPIO_PATH);
             println!("gpio number = {:?}", gpio);
             println!("mode = {:?}", mode);
-
             {
                 let mut export_file = match OpenOptions::new().write(true).open(format!("{}export", GPIO_PATH)) {
                     Ok(f) => f,
                     Err(e) => panic!("file error: {}", e),
                 };
-
-               match export_file.write_all(format!("{}", gpio).as_bytes()) {
-                   Err(why) => {
-                       panic!("couldn't initialize gpio {}: {}",gpio , why.description())
-                   },
-                   Ok(_) => println!("gpio {} initialized", gpio),
-               }
+                match export_file.write_all(format!("{}", gpio).as_bytes()) {
+                    Err(why) => {
+                       panic!("couldn't initialize gpio {}: {}",gpio , why)
+                    },
+                    Ok(_) => println!("gpio {} initialized", gpio),
+                }
            }
-           {
+           self.set_mode(mode)
+        }
+    }
 
-           }
-           GPIO {pin: gpio, mode: mode}
+    impl Drop for GPIO{
+        fn drop(&mut self){
+            let mut unexport = match OpenOptions::new().write(true).open(format!("{}unexport", GPIO_PATH)) {
+                Ok(f) => f,
+                Err(e) => panic!("file error: {}", e),
+            };
+            match unexport.write_all(format!("{}", self.pin).as_bytes()) {
+                Err(why) => {
+                   panic!("couldn't close gpio {}: {}",self.pin , why)
+                },
+                Ok(_) => println!("gpio {} closed", self.pin),
+            }
+        }
+    }
+
+    impl Read for GPIO {
+        fn read(&mut self, buf: &mut [u8]) -> Result<usize>{
+            let value = match OpenOptions::new().read(true).open(format!("{}gpio{}/value", GPIO_PATH, self.pin)) {
+                Ok(f) => f,
+                Err(e) => panic!("file error: {}", e),
+            };
+            let mut buffer = vec![];
+            match value.take(1).read_to_end(&mut buffer ){
+                Ok(n) => {
+                    buf[0] = buffer[0];
+                    Ok(n)
+                },
+                Err(e) => panic!(e),
+            }
         }
     }
 }
