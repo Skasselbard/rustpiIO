42d425cb624aaaaf9d23b3f8a621380d0f5b928d
tom <tm219@uni-rostock.de>
2017-08-13 11:05:52 +0200

    returns are now references and try! makro reduces complexity

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,7 @@
 pub mod rustpi_io{
     use std::fs::OpenOptions;
     use std::io::prelude::*;
-    use std::error::Error;
     use std::io::Result;
-
 
     static GPIO_PATH: &'static str = "/sys/class/gpio/";
 
@@ -20,55 +18,32 @@ pub mod rustpi_io{
     }
 
     impl GPIO{
-        pub fn gpio(self) -> u8 { self.pin }
+        pub fn gpio(&self) -> u8 { self.pin }
 
-        pub fn current_mode(self) -> GPIOMode {
+        pub fn current_mode(&self) -> GPIOMode {
             self.mode
         }
 
-        pub fn set_mode(self, mode: GPIOMode) -> Result<Self>{
-            let mut direction_file = match OpenOptions::new().write(true).open(format!("{}gpio{}/direction", GPIO_PATH, self.pin)) {
-                Ok(f) => f,
-                Err(e) => panic!("file error: {}", e),
-            };
-
+        pub fn set_mode(&mut self, mode: GPIOMode) -> Result<&mut Self>{
+            let mut direction = OpenOptions::new().write(true).open(format!("{}gpio{}/direction", GPIO_PATH, self.pin))?;
             match mode{
-                GPIOMode::Read   => {
-                    match direction_file.write_all("in".as_bytes()) {
-                        Err(why) => {
-                            panic!("couldn't open direction file for gpio {}: {}",self.pin , why.description())
-                        },
-                        Ok(_) => println!("gpio {} set to read mode", self.pin),
-                    }
-                }
-                GPIOMode::Write   => {
-                    match direction_file.write_all("out".as_bytes()) {
-                        Err(why) => {
-                            panic!("couldn't open direction file for gpio {}: {}",self.pin , why.description())
-                        },
-                        Ok(_) => println!("gpio {} set to write mode", self.pin),
-                    }
-                }
+                GPIOMode::Read   => try!(direction.write_all("in".as_bytes())),
+                GPIOMode::Write   => try!(direction.write_all("out".as_bytes())),
             };
-            Ok(Self { pin: self.pin, mode: mode})
+            self.mode = mode;
+            Ok(self)
         }
 
         pub fn init(gpio: u8, mode: GPIOMode) -> Result<Self> {
             println!("gpio number = {:?}", gpio);
             println!("mode = {:?}", mode);
             {
-                let mut export_file = match OpenOptions::new().write(true).open(format!("{}export", GPIO_PATH)) {
-                    Ok(f) => f,
-                    Err(e) => panic!("file error: {}", e),
-                };
-                match export_file.write_all(format!("{}", gpio).as_bytes()) {
-                    Err(why) => {
-                       panic!("couldn't initialize gpio {}: {}",gpio , why)
-                    },
-                    Ok(_) => println!("gpio {} initialized", gpio),
-                }
-           }
-           Ok(GPIO{pin: gpio, mode: mode}.set_mode(mode)?)
+                let mut export_file = OpenOptions::new().write(true).open(format!("{}export", GPIO_PATH))?;
+                export_file.write_all(format!("{}", gpio).as_bytes())?;
+            }
+            let mut result = GPIO{pin: gpio, mode: mode};
+            result.set_mode(mode)?;
+            Ok(result)
         }
     }
 
@@ -89,18 +64,11 @@ pub mod rustpi_io{
 
     impl Read for GPIO {
         fn read(&mut self, buf: &mut [u8]) -> Result<usize>{
-            let mut value = match OpenOptions::new().read(true).open(format!("{}gpio{}/value", GPIO_PATH, self.pin)) {
-                Ok(f) => f,
-                Err(e) => panic!("file error: {}", e),
-            };
+            let mut value = try!(OpenOptions::new().read(true).open(format!("{}gpio{}/value", GPIO_PATH, self.pin)));
             let mut buffer = vec![];
-            match value.read_to_end(&mut buffer ){
-                Ok(n) => {
-                    buf[0] = buffer[0];
-                    Ok(n)
-                },
-                Err(e) => panic!(e),
-            }
+            try!(value.read_to_end(&mut buffer ));
+            buf[0] = buffer[0];
+            Ok(buffer.len())
         }
     }
 }
