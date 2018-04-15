744fcd009f8d8104a1c5792c5333570679f28b38
Tom <tom.meyer89@gmail.com>
2018-04-15 10:40:54 +0200

    license

--- src/i2c.rs
+++ src/i2c.rs
@@ -1,3 +1,22 @@
+// This file is part of RustpiIO.
+//
+// Copyright 2018
+//
+// Contributors: Tom Meyer
+//
+// RustpiIO is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RustpiIO is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RustpiIO.  If not, see <http://www.gnu.org/licenses/>
+
 use std;
 use i2c_linux::{I2c, Message, ReadFlags, WriteFlags};
 use internal_i2c;
