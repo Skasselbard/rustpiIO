56cf21baac649592b679b7e23ccfa705de3292e8
Tom <tom.meyer89@gmail.com>
2018-04-22 10:15:56 +0200

    changed i2c interface and added smbus functions

--- src/i2c.rs
+++ src/i2c.rs
@@ -18,10 +18,10 @@
 // along with RustpiIO.  If not, see <http://www.gnu.org/licenses/>
 
 use std;
-use i2c_linux::I2c;
+pub use i2c_linux::{Message, ReadFlags, WriteFlags, Functionality, ReadWrite};
 use i2c_linux;
-pub use internal_i2c::{Address, BlockTransfer, BulkTransfer, Master, Message, ReadFlags,
-                       ReadWrite, WriteFlags};
+use i2c_linux::{I2c};
+use std::time::Duration;
 
 pub struct I2CPi {
     inner: I2c<std::fs::File>,
@@ -35,26 +35,35 @@ impl I2CPi {
         };
         Ok(I2CPi { inner: i2c })
     }
-    pub fn i2c_transfer(&mut self, messages: &mut [i2c_linux::Message]) -> Result<(), <I2CPi as Master>::Error> {
-        self.inner.i2c_transfer(messages)
-    }
-}
 
-impl Master for I2CPi {
-    type Error = std::io::Error;
-}
-
-impl Address for I2CPi {
-    fn set_slave_address(&mut self, addr: u16, tenbit: bool) -> Result<(), Self::Error> {
+    pub fn slave_address(&mut self, addr: u16, tenbit: bool) -> std::io::Result<()>{
         self.inner.smbus_set_slave_address(addr, tenbit)
     }
-}
 
-impl BlockTransfer for I2CPi {
-    fn i2c_read_block_data(&mut self, command: u8, value: &mut [u8]) -> Result<usize, Self::Error> {
+    pub fn functionality(&self) -> std::io::Result<Functionality> {
+        self.inner.i2c_functionality()
+    }
+
+    pub fn transfer(&mut self, messages: &mut [Message]) -> std::io::Result<()> {
+        self.inner.i2c_transfer(messages)
+    }
+
+    pub fn transfer_flags(&self) -> std::io::Result<(ReadFlags, WriteFlags)>{
+        self.inner.i2c_transfer_flags()
+    }
+
+    pub fn set_retries(&self, value: usize) -> std::io::Result<()>{
+        self.inner.i2c_set_retries(value)
+    }
+
+    pub fn set_timeout(&self, duration: Duration) -> std::io::Result<()>{
+        self.inner.i2c_set_timeout(duration)
+    }
+    
+    pub fn read_block_data(&mut self, command: u8, value: &mut [u8]) -> std::io::Result<usize> {
         self.inner.i2c_read_block_data(command, value)
     }
-    fn i2c_write_block_data(&mut self, command: u8, value: &[u8]) -> Result<(), Self::Error> {
+    pub fn write_block_data(&mut self, command: u8, value: &[u8]) -> std::io::Result<()> {
         self.inner.i2c_write_block_data(command, value)
     }
 }
@@ -66,6 +75,105 @@ impl std::io::Read for I2CPi {
 }
 
 impl std::io::Write for I2CPi {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.inner.write(buf)
+    }
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.inner.flush()
+    }
+}
+
+pub struct SMBusPi {
+    inner: I2c<std::fs::File>,
+}
+
+impl SMBusPi {
+    pub fn new() -> std::io::Result<I2CPi> {
+        let i2c = match I2c::from_path("/dev/i2c-0") {
+            Ok(device) => device,
+            Err(_) => I2c::from_path("/dev/i2c-1")?,
+        };
+        Ok(I2CPi { inner: i2c })
+    }
+
+    pub fn slave_address(&mut self, addr: u16, tenbit: bool) -> std::io::Result<()>{
+        self.inner.smbus_set_slave_address(addr, tenbit)
+    }
+
+    pub fn functionality(&self) -> std::io::Result<Functionality> {
+        self.inner.i2c_functionality()
+    }
+
+    pub fn transfer(&mut self, messages: &mut [Message]) -> std::io::Result<()> {
+        self.inner.i2c_transfer(messages)
+    }
+
+    pub fn transfer_flags(&self) -> std::io::Result<(ReadFlags, WriteFlags)>{
+        self.inner.i2c_transfer_flags()
+    }
+
+    pub fn set_retries(&self, value: usize) -> std::io::Result<()>{
+        self.inner.i2c_set_retries(value)
+    }
+
+    pub fn set_timeout(&self, duration: Duration) -> std::io::Result<()>{
+        self.inner.i2c_set_timeout(duration)
+    }
+
+
+
+    pub fn write_bit(&mut self, value: ReadWrite) -> std::io::Result<()> {
+        self.inner.smbus_write_quick( value)
+    }
+
+    pub fn read_primitive(&mut self) -> std::io::Result<u8> {
+        self.inner.smbus_read_byte()
+    }
+
+    pub fn write_primitive(&mut self, value: u8) -> std::io::Result<()> {
+        self.inner.smbus_write_byte(value)
+    }
+
+    pub fn read_byte(&mut self, command: u8) -> std::io::Result<u8> {
+        self.inner.smbus_read_byte_data(command)
+    }
+
+    pub fn write_byte(&mut self, command: u8, value: u8) -> std::io::Result<()> {
+        self.inner.smbus_write_byte_data(command, value)
+    }
+
+    pub fn read_word(&mut self, command: u8) -> std::io::Result<u16> {
+        self.inner.smbus_read_word_data(command)
+    }
+
+    pub fn write_word(&mut self, command: u8, value: u16) -> std::io::Result<()> {
+        self.inner.smbus_write_word_data(command, value)
+    }
+
+    pub fn process_call(&mut self, command: u8, value: u16) -> std::io::Result<u16> {
+        self.inner.smbus_process_call(command, value)
+    }
+
+    pub fn read_block_data(&mut self, command: u8, values: &mut [u8]) -> std::io::Result<usize> {
+        self.inner.smbus_read_block_data(command, values)
+    }
+
+    pub fn write_block_data(&mut self, command: u8, value: &[u8]) -> std::io::Result<()>{
+        self.inner.smbus_write_block_data(command, value)
+    }
+
+    pub fn block_process_call(&mut self, command: u8, write: &[u8], read: &mut [u8]) -> std::io::Result<usize> {
+        self.inner.smbus_block_process_call(command, write, read)
+    }
+}
+
+impl std::io::Read for SMBusPi {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        self.inner.read(buf)
+    }
+}
+
+impl std::io::Write for SMBusPi {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         self.inner.write(buf)
     }
